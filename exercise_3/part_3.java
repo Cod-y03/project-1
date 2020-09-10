c4673958bc16875c56f28665f39ac52a807517f8
Cody Mailey <cmailey03@gmail.com>
2020-09-09 22:25:49 -0600

    done with exercise 10 part 3

---
+++ exercise_3/part_3.java
@@ -0,0 +1,11 @@
+public class part_3
+{
+    public static void main(String[] args )
+    {
+        double degrees = 30;
+        double rad = degrees * Math.PI/180;
+
+        System.out.println(degrees + " degrees is: " + rad);
+
+    }
+}
