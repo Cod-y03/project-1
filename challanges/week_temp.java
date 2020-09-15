7523263a461fff3d871e8a9dc2a3e26237874942
Cody Mailey <cmailey03@gmail.com>
2020-09-15 11:26:35 -0600

    start work on discountPrices

--- challanges/week_temp.java
+++ challanges/week_temp.java
@@ -1,4 +1,38 @@
+import java.util.Scanner;
+
 public class week_temp 
 {
-    
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System .in); 
+        
+        System.out.println("Please enter the temp for Monday.");
+        int monday = scanner.nextInt();
+
+        System.out.println("Please enter the temp for Tuesday.");
+        int tuesday = scanner.nextInt();
+
+
+        System.out.println("Please enter the temp for Wednesday.");
+        int wednesday = scanner.nextInt();
+
+
+        System.out.println("Please enter the temp for Thursday.");
+        int thursday = scanner.nextInt();
+
+
+        System.out.println("Please enter the temp for Friday.");
+        int friday = scanner.nextInt();
+
+
+        System.out.println("Please enter the temp for Saterday.");
+        int saterday = scanner.nextInt();
+
+
+        System.out.println("Please enter the temp for Sunday.");
+        int sunday = scanner.nextInt();
+
+        double avg = ((monday + tuesday +wednesday + thursday + friday + saterday + sunday) / 7.0);
+
+        System.out.println("The average temp is " + avg);
+    }
 }
---
+++ chapter_16/DiscountPrices.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DiscountPrices {
+
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        double cost = scanner.nextInt();
+
+        if (cost > 10) {
+            cost = cost * 0.10;
+        }
+        System 
+            
+    }
+}
