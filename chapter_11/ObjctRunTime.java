0f9cad7f9de3c39996cfa8298e916295c20a87de
Cody Mailey <cmailey03@gmail.com>
2020-09-25 10:42:24 -0600

    part way though quiz

---
+++ chapter_11/ObjctRunTime.java
@@ -0,0 +1,8 @@
+public class ObjctRunTime {
+
+    public static void main(String[] args) {
+        String str = "Hello world";
+        sec = new String ("Hey");
+        System.out.println(str);
+    }
+}
