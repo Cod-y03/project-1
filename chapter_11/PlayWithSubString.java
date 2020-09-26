0d3cbcd5b056969d0a6214058efb8ee0c7e48eb0
Cody Mailey <cmailey03@gmail.com>
2020-09-25 23:22:13 -0600

    done with all of chapter 11

---
+++ chapter_11/PlayWithSubString.java
@@ -0,0 +1,9 @@
+public class PlayWithSubString
+{
+    public static void main(String[] args) {
+        String first = new String("Golf is a very lame game,");
+
+        String sub = first.substring(8);
+        System.out.println(sub);
+    }
+}
---
+++ chapter_11/StringMethodtoANewString.java
@@ -0,0 +1,13 @@
+public class StringMethodtoANewString 
+{
+    public static void main(String[] args) {
+        String first = new String("     I   want to say hello world     ");
+        System.out.println(first);
+        String trimed;
+        trimed = first.trim();
+
+        System.out.println(trimed);
+    }
+
+
+}
