00cdad614a53830c77e9a83d05ef4802df25d01d
Cody Mailey <cmailey03@gmail.com>
2020-10-20 08:17:17 -0600

    done with 20 working on cubes and squares

---
+++ chapter_21/AddingUpSquaresAndCubes.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class AddingUpSquaresAndCubes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter N: ");
+        int limit = scanner.nextInt();
+        while (limit > 0) {
+            
+        }
+
+    }
+}
