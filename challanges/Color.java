92a48e46809814543b3ee88289951dd59c296b7d
Cody Mailey <cmailey03@gmail.com>
2020-10-22 11:19:34 -0600

    Working on Color using classes

---
+++ challanges/Color.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Color 
+{
+    Random randomNumber = new Random();
+    int max = 255;
+    private int red;
+    private int green;
+    private int blue;
+
+    public Color( int red, int green, int blue)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+
+    }
+
+}
---
+++ challanges/ColorTest.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class ColorTest
+{
+    public static void main(String[] args) {
+
+        boolean setColor = true;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        double
+        Color color = new Color(red, green, blue);
+
+
+    }
+
+}
