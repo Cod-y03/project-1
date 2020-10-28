f1b2a7f4b83f8c23d590441c9a75af0af3dc996f
Cody Mailey <cmailey03@gmail.com>
2020-10-28 11:12:28 -0600

    working on lighten and darken

--- challanges/Color.java
+++ challanges/Color.java
@@ -43,5 +43,67 @@ public class Color
 
     //setters and getters
 
+    public void getColor() {
+        System.out.println("Color: " + this.red + ", " + this.green + ", " + this.blue);
+    }
 
+    public void setRandom() {
+        this.red = randomNumber.nextInt(max);
+        this.green = randomNumber.nextInt(max);
+        this.blue = randomNumber.nextInt(max);
+    }
+
+    //abilities
+
+    public void lighten(int percent) {
+        int raise = 255 / percent;
+
+        this.red = this.red + raise;
+        if (this.red > 255) {
+            this.red = 255;
+        }
+        if (red < 0) {
+            this.red = 0;
+        }
+        this.green = this.green + raise;
+        if (this.green > 255) {
+            this.green = 255;
+        }
+        if (this.green < 0) {
+            this.green = 0;
+        }
+        this.blue = this.blue + raise;
+        if (this.blue > 255) {
+            this.blue = 255;
+        }
+        if (this.blue < 0) {
+            this.blue = 0;
+        }    
+    }
+
+    public void darken(int percent) {
+        int lower = 255 / percent;
+
+        this.red = this.red - lower;
+        if (this.red > 255) {
+            this.red = 255;
+        }
+        if (red < 0) {
+            this.red = 0;
+        }
+        this.green = this.green - lower;
+        if (this.green > 255) {
+            this.green = 255;
+        }
+        if (this.green < 0) {
+            this.green = 0;
+        }
+        this.blue = this.blue - lower;
+        if (this.blue > 255) {
+            this.blue = 255;
+        }
+        if (this.blue < 0) {
+            this.blue = 0;
+        }    
+    }
 }
--- challanges/ColorTest.java
+++ challanges/ColorTest.java
@@ -3,8 +3,12 @@ public class ColorTest
     public static void main(String[] args) {
 
         Color color = new Color(23, 12, 120);
-
-
+        color.getColor();
+        Color color2 = new Color();
+        color2.getColor();
+        
+        color2.setRandom();
+        color2.getColor();
     }
 
 }
