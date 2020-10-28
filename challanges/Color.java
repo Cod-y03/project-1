db1be2997af7e20da53f51075821e618a3540b86
Cody Mailey <cmailey03@gmail.com>
2020-10-28 10:44:28 -0600

    adding setters and getters in color

--- challanges/Color.java
+++ challanges/Color.java
@@ -10,14 +10,38 @@ public class Color
     private int green;
     private int blue;
 
+    //constructers
+    public Color() {
+        this.red = randomNumber.nextInt(max);
+        this.green = randomNumber.nextInt(max);
+        this.blue = randomNumber.nextInt(max);
+        }
     public Color( int red, int green, int blue)
     {
         this.red = red;
+        if (red > 255) {
+            this.red = 255;
+        }
+        if (red < 0) {
+            this.red = 0;
+        }
         this.green = green;
+        if (green > 255) {
+            this.green = 255;
+        }
+        if (green < 0) {
+            this.green = 0;
+        }
         this.blue = blue;
-
-        
-
+        if (blue > 255) {
+            this.blue = 255;
+        }
+        if (blue < 0) {
+            this.blue = 0;
+        }
     }
+
+    //setters and getters
+
 
 }
--- challanges/ColorTest.java
+++ challanges/ColorTest.java
@@ -1,16 +1,8 @@
-import java.util.Random;
-
 public class ColorTest
 {
     public static void main(String[] args) {
 
-        boolean setColor = true;
-        int red = 0;
-        int green = 0;
-        int blue = 0;
-
-        double
-        Color color = new Color(red, green, blue);
+        Color color = new Color(23, 12, 120);
 
 
     }
