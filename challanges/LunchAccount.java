500645c24af0838960c3f5b0f9fdd6851ecc56c0
Cody Mailey <cmailey03@gmail.com>
2020-11-16 11:30:28 -0700

    done with all requierments for Lunch Account

--- challanges/LunchAccount.java
+++ challanges/LunchAccount.java
@@ -1,8 +1,4 @@
-import sun.net.www.content.text.plain;
-
 import java.util.Random;
-
-import org.graalvm.compiler.nodes.debug.BlackholeNode;
 
 public class LunchAccount
 {
@@ -10,34 +6,92 @@ public class LunchAccount
     private int id = 0;
     private double balance = 0;
     private double spent = 0;
+    private int lunchCount = 0;
+    private int firstAddToBalance = 1;
+    double burger = 3.99;
+    double chips = 2.99; 
+    double fries = 1.50; 
+    Random randomGenerator = new Random();
     int numb = 0;
 
-
-        public LunchAccount(String name, double balance) {
+    
+    public LunchAccount(String name, double balance) {
             this.name = name;
             this.balance = balance;
+            this.id = makeID();
             numb = numb + 1;
-            if (numb < 100) {
-                this.balance = this.balance + 20;
-            }
+            extra();
         }
-
+        
         public LunchAccount(String name) {
             this.name = name;
+            this.id = makeID();
             numb = numb + 1;
-            if (numb < 100) {
-                this.balance = this.balance + 20;
+            extra();
+        }
+        
+        public void extra() {//to check if they would get the extra money for adding to the account
+           if (firstAddToBalance > 0) { 
+                if (numb < 100) { 
+                    this.balance = this.balance + 20;
+                }
             }
+            firstAddToBalance = 0;   
         }
 
-        public String buyLunch() {
-            if ((this.balance - 5) > 0 ) {
-                this.balance = this.balance - 5;//lunch cost
-                this.spent = this.spent + 5;
-                return "lunch purchaced, cost: $5";
+        public int makeID() {
+            int max = 99; 
+            return (randomGenerator.nextInt(max) * 100) + numb; //uses the count of acounts to unsure that its unike.
+        }
+
+        public int getLunchCount() {
+            return lunchCount;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void addBalance(int add) {
+            balance += add;
+            extra();
+        }
+
+        public String buyLunch(int first, int second) {
+            if ((this.balance - lunchTotal(first, second)) > 0 ) {
+                this.balance = this.balance - lunchTotal(first, second);//lunch cost
+                this.spent = this.spent + lunchTotal(first, second);
+                this.lunchCount += 1;
+                return "lunch purchaced, cost: " + lunchTotal(first, second);
             } else {
                 return "not enough money";
             }
         }
 
+        public double lunchTotal(int first, int second) {
+            double sum = 0;
+            if (first == 1) {
+                sum += burger;
+            }else if (first == 2) {
+                sum += fries;
+            }else if (first == 3) {
+                sum += chips;
+            }
+            if (second == 1) {
+                sum += burger;
+            }else if (second == 2) {
+                sum += fries;
+            }else if (second == 3) {
+                sum += chips;
+            }
+            return sum;
+        }
 }
--- challanges/LunchTest.java
+++ challanges/LunchTest.java
@@ -1,6 +1,31 @@
 public class LunchTest
 {
     public static void main(String[] args) {
-        
+         LunchAccount sam = new LunchAccount("Sam");
+         LunchAccount gabe = new LunchAccount("Gabe", 5 ); 
+         LunchAccount adam = new LunchAccount("Adam", 5); 
+
+        sam.addBalance(10);
+        System.out.println(sam.buyLunch(1, 2));//numbers to distingush items, 1 = burger, 2 = fries, 3 = chips
+
+
+        System.out.println("gabe " + gabe.getBalance());
+        System.out.println(gabe.buyLunch(1, 3));
+        System.out.println(gabe.getBalance());
+        System.out.println(gabe.buyLunch(1, 1));
+        System.out.println(gabe.getBalance());
+        System.out.println(gabe.buyLunch(1, 3));
+        System.out.println(gabe.getBalance());
+        System.out.println(gabe.buyLunch(2, 3));
+        System.out.println(gabe.getBalance());
+
+        adam.addBalance(10);
+        System.out.println("adam " + adam.getBalance());
+        System.out.println(adam.buyLunch(1, 3));
+        System.out.println(adam.getBalance());
+        adam.addBalance(10);
+        System.out.println(adam.getBalance());
+        System.out.println(adam.buyLunch(1, 2));
+
     }
 }
